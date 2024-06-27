456a2cc446f731ccd48832e1dc8f22b3e77dc513
Rayduxx <moussa.ala@esprit.tn>
2024-06-27 13:41:54 +0100

    updates

--- src/main/java/tn/bfpme/controllers/DemandeDepController.java
+++ src/main/java/tn/bfpme/controllers/DemandeDepController.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 import tn.bfpme.models.Conge;
 import tn.bfpme.models.Statut;
 import tn.bfpme.models.TypeConge;
+import tn.bfpme.models.Utilisateur;
 import tn.bfpme.utils.MyDataBase;
 import tn.bfpme.utils.SessionManager;
 
@@ -16,34 +17,28 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.temporal.ChronoUnit;
 
 public class DemandeDepController {
-    @FXML private Label CongerPerson;
+    @FXML private Label CongePerson;
     @FXML private Label labelDD;
     @FXML private Label labelDF;
     @FXML private Label labelDesc;
     @FXML private Label labelJours;
     @FXML private Label labelType;
     Connection cnx = MyDataBase.getInstance().getCnx();
+    private Conge conge;
+    private Utilisateur user;
 
-    public void setData(Conge conge) {
-        /*String qry = "SELECT `ID_User` FROM `conge` WHERE `ID_Conge`=? ";
-        try {
-            PreparedStatement stm = cnx.prepareStatement(qry);
-            stm.setInt(1, conge.getIdConge());
-            ResultSet rs = stm.executeQuery();
-            while (rs.next()) {
-                String qry1 = "SELECT * FROM `utilisateur` WHERE `ID_User`=?";
-                PreparedStatement stm1 = cnx.prepareStatement(qry1);
-                stm1.setInt(1, conge.getIdConge());
-                ResultSet rs1 = stm.executeQuery();
-                while (rs1.next()) {
-                    CongerPerson.setText(rs1.getString("Nom")+""+rs1.getString("Prenom"));
-                }
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }*/
+    public void setData(Conge conge, Utilisateur user) {
+        this.conge = conge;
+        this.user = user;
+        CongePerson.setText(user.getNom() + " " + user.getPrenom());
+        labelDD.setText(String.valueOf(conge.getDateDebut()));
+        labelDF.setText(String.valueOf(conge.getDateFin()));
+        labelDesc.setText(conge.getDescription());
+        labelType.setText(String.valueOf(conge.getTypeConge()));
+        labelJours.setText(String.valueOf(ChronoUnit.DAYS.between(conge.getDateDebut(), conge.getDateFin()))+" Jours");
     }
 
     @FXML void AfficherCongFichier(ActionEvent event) {
--- src/main/java/tn/bfpme/controllers/UserCarteController.java
+++ src/main/java/tn/bfpme/controllers/UserCarteController.java
@@ -1,31 +1,32 @@
 package tn.bfpme.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import tn.bfpme.models.Conge;
 import tn.bfpme.models.Utilisateur;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class UserCarteController {
-    @FXML
-    private Pane UserCard;
-    @FXML
-    private Label email_card;
-    @FXML
-    private Label nomprenom_card;
-    @FXML
-    private ImageView pdp_card;
-    @FXML
-    private Label periode_card;
-    @FXML
-    private Label statut_card;
-    @FXML
-    private Label type_card;
+    @FXML private Pane UserCard;
+    @FXML private Label email_card;
+    @FXML private Label nomprenom_card;
+    @FXML private ImageView pdp_card;
+    @FXML private Label periode_card;
+    @FXML private Label statut_card;
+    @FXML private Label type_card;
 
     private Conge conge;
     private Utilisateur user;
@@ -34,7 +35,6 @@ public class UserCarteController {
         System.out.println("Setting data for user: " + user.getNom() + " " + user.getPrenom());
         this.conge = conge;
         this.user = user;
-
         String imagePath = user.getImage();
         if (imagePath != null) {
             try {
@@ -48,14 +48,31 @@ public class UserCarteController {
         } else {
             System.err.println("Image path is null for user: " + user);
         }
+
         nomprenom_card.setText(user.getNom() + " " + user.getPrenom());
         email_card.setText(user.getEmail());
         periode_card.setText("De " + conge.getDateDebut() + " à " + conge.getDateFin());
         statut_card.setText(String.valueOf(conge.getStatut()));
         type_card.setText(String.valueOf(conge.getTypeConge()));
         UserCard.setStyle("-fx-border-radius: 5px; -fx-border-color: #808080;");
-
         System.out.println("Card data set for user: " + user.getNom() + " " + user.getPrenom());
+    }
+    @FXML
+    void AfficherDemande(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/DemandeDep.fxml"));
+            Parent root = loader.load();
+            Stage newStage = new Stage();
+            DemandeDepController DemDepC = loader.getController();
+            DemDepC.setData(conge, user);
+            newStage.setTitle("Demande de congé");
+            newStage.setScene(new Scene(root));
+            newStage.initModality(Modality.WINDOW_MODAL);
+            newStage.initOwner(((Node) event.getSource()).getScene().getWindow());
+            newStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
