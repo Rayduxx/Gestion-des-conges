bf39ed5e272880eb3f9e8509a1b7e9a5afd34084
Slimbentanfous1 <slimbentanfous2002@gmail.com>
2024-07-08 20:03:13 +0100

    Implementing (Solde Congé system)

---
+++ src/main/java/tn/bfpme/controllers/SoldeLogicController.java
@@ -0,0 +1,28 @@
+package tn.bfpme.controllers;
+
+/*import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class SoldeLogicController {
+    public static int calculateSoldeMaternite(LocalDate creationDate) {
+        // Example logic: 30 days for each full year since creation
+        return (int) ChronoUnit.YEARS.between(creationDate, LocalDate.now()) * 30;
+    }
+
+    public static int calculateSoldeAnnuelle(LocalDate creationDate) {
+        // Example logic: 20 days for each full year since creation
+        return (int) ChronoUnit.YEARS.between(creationDate, LocalDate.now()) * 20;
+    }
+
+    public static int calculateSoldeExceptionnel(LocalDate creationDate) {
+        // Example logic: 5 days for each full year since creation
+        return (int) ChronoUnit.YEARS.between(creationDate, LocalDate.now()) * 5;
+    }
+
+    public static int calculateSoldeMaladie(LocalDate creationDate) {
+        // Example logic: 10 days for each full year since creation
+        return (int) ChronoUnit.YEARS.between(creationDate, LocalDate.now()) * 10;
+
+    }
+}
+*/
