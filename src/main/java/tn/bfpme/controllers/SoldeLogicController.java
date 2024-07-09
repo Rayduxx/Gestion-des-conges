f7ed1d0502af0bd21e59b7c2787c281aad47ca28
Slimbentanfous1 <slimbentanfous2002@gmail.com>
2024-07-09 11:33:20 +0100

    Implementing (Solde Congé system)

--- src/main/java/tn/bfpme/controllers/SoldeLogicController.java
+++ src/main/java/tn/bfpme/controllers/SoldeLogicController.java
@@ -1,28 +1,34 @@
 package tn.bfpme.controllers;
 
-/*import java.time.LocalDate;
+import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class SoldeLogicController {
     public static int calculateSoldeMaternite(LocalDate creationDate) {
-        // Example logic: 30 days for each full year since creation
+        if (creationDate == null) {
+            creationDate = LocalDate.now();
+        }
         return (int) ChronoUnit.YEARS.between(creationDate, LocalDate.now()) * 30;
     }
 
-    public static int calculateSoldeAnnuelle(LocalDate creationDate) {
-        // Example logic: 20 days for each full year since creation
-        return (int) ChronoUnit.YEARS.between(creationDate, LocalDate.now()) * 20;
+    public static double calculateSoldeAnnuelle(LocalDate creationDate) {
+        if (creationDate == null) {
+            creationDate = LocalDate.now();
+        }
+        return (double) ChronoUnit.YEARS.between(creationDate, LocalDate.now()) * 2.5;
     }
 
     public static int calculateSoldeExceptionnel(LocalDate creationDate) {
-        // Example logic: 5 days for each full year since creation
+        if (creationDate == null) {
+            creationDate = LocalDate.now();
+        }
         return (int) ChronoUnit.YEARS.between(creationDate, LocalDate.now()) * 5;
     }
 
     public static int calculateSoldeMaladie(LocalDate creationDate) {
-        // Example logic: 10 days for each full year since creation
+        if (creationDate == null) {
+            creationDate = LocalDate.now();
+        }
         return (int) ChronoUnit.YEARS.between(creationDate, LocalDate.now()) * 10;
-
     }
 }
-*/
